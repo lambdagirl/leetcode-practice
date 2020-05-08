edc01dee316d25244ddebfdfcd7ee0245e288306
Peizhi Zheng <parisa1206@gmail.com>
2020-05-07 21:43:41 -0700

    dfs+global

---
+++ cousins-in-binary-tree.py
@@ -0,0 +1,63 @@
+# In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+# Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+# We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+# Return true if and only if the nodes corresponding to the values x and y are cousins.
+
+# Example 1:
+
+# Input: root = [1,2,3,4], x = 4, y = 3
+# Output: false
+# Example 2:
+
+# Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+# Output: true
+# Example 3:
+
+# Input: root = [1,2,3,null,4], x = 2, y = 3
+# Output: false
+
+# Note:
+
+# The number of nodes in the tree will be between 2 and 100.
+# Each node has a unique integer value from 1 to 100.
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        xstate = {'depth': 0, 'parent': None}
+        ystate = {'depth': 0, 'parent': None}
+
+        def dfs_traverse(root, x, y, depth, parent):
+            if not root:
+                return None
+            if root.val == x:
+                xstate['depth'] = depth
+                xstate['parent'] = parent
+            if root.val == y:
+                ystate['depth'] = depth
+                ystate['parent'] = parent
+            traverse(root.left, x, y, depth + 1, root)
+            traverse(root.right, x, y, depth + 1, root)
+
+        traverse(root, x, y, 0, None)
+        return xstate['depth'] == ystate[
+            'depth'] and xstate['parent'] != ystate['parent']
