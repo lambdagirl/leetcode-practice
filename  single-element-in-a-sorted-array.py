7ae75e1bf50a855cb4552e49273a5b25fcf0acb8
Peizhi Zheng <parisa1206@gmail.com>
2020-05-12 20:57:55 -0700

    medium

---
+++  single-element-in-a-sorted-array.py
@@ -0,0 +1,43 @@
+# You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+
+
+# Example 1:
+
+# Input: [1,1,2,3,3,4,4,8,8]
+# Output: 2
+# Example 2:
+
+# Input: [3,3,7,7,10,11,11]
+# Output: 10
+# Input: [3,3,7,7,10,10,11,12,12]
+# Output: 10
+
+# Note: Your solution should run in O(log n) time and O(1) space.
+
+# Approach 1: Brute Force; O(N); spaceO(1)
+def singleNonDuplicate(self, nums: List[int]) -> int:
+    for i in range(len(nums)-2, 2):
+        if nums[i] != nums[i+1]:
+            return nums[i]
+    return nums[-1]
+
+# Approach 2: Binary Search; O(logN); spaceO(1)
+
+
+def singleNonDuplicate(self, nums: List[int]) -> int:
+    l, r = 0, len(nums)-1
+    while l < r:
+        mid = (l + r) // 2
+        if nums[mid] == nums[mid - 1]:
+            if (r-mid) % 2 == 0:
+                r = mid - 2
+            else:
+                l = mid + 1
+        elif nums[mid] == nums[mid + 1]:
+            if (r-mid) % 2 == 0:
+                l = mid + 2
+            else:
+                r = mid - 1
+        else:
+            return nums[mid]
+    return nums[l]
