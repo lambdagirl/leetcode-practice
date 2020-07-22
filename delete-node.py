679f13331eebe12e965ac8e61f3fd9bec64ca710
Peizhi Zheng <pzheng16@asu.edu>
2020-07-22 15:33:31 -0700

    add side effect

--- delete-node.py
+++ delete-node.py
@@ -6,9 +6,12 @@ class LinkedListNode(object):
 
 #modify the current node instead of deleting it
 def delete_node(node):
-    node.value = node.next.value
-    node.next = node.next.next
-    
+    if node.next:
+        node.value = node.next.value
+        node.next = node.next.next
+    else:
+        raise Exception("Can't delete the last node!")
+
 a = LinkedListNode('A')
 b = LinkedListNode('B')
 c = LinkedListNode('C')
@@ -18,3 +21,8 @@ b.next = c
 
 delete_node(b)
 print(a.next.value)
+
+#side effect
+#1.First, it doesn't work for deleting the last node in the list.
+#2.Any references to the input node have now effectively been reassigned to its next node.
+#3.If there are pointers to the input node's original next node, those pointers now point to a "dangling" node
