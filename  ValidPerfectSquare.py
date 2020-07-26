dc6b0668dd5a50e82fa82c352c80561a615e4664
Peizhi Zheng <pzheng16@asu.edu>
2020-07-25 18:55:06 -0700

    update

---  ValidPerfectSquare.py
+++  ValidPerfectSquare.py
@@ -1,5 +1,4 @@
 # Given a positive integer num, write a function which returns True if num is a perfect square else False.
-
 # Note: Do not use any built-in library function such as sqrt.
 
 # Example 1:
@@ -10,20 +9,23 @@
 
 # Input: 14
 # Output: false
-
-
 #bianry search
-class Solution:
-    def isPerfectSquare(self, num: int) -> bool:
-        if num <= 2:
+'''
+x * x = 16 => True
+the square root aa is always less than extrm{num} / 2num/2 and greater than 1:
+'''
+def isPerfectSquare(num):
+    if num == 1:
+        return True
+    l, r = 2, num // 2
+    while l <= r:
+        mid = l + (r-l) // 2
+        if mid * mid == num:
             return True
-        l, r = 2, num // 2
-        while l <= r:
-            mid = (l + r) // 2
-            if mid * mid == num:
-                return True
-            elif mid * mid < num:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return False
+        elif mid * mid < num:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return False
+    
+print(isPerfectSquare(2))
