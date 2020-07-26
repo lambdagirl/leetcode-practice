4339972727bbb40e79a7b4f312b9fb004d951fd8
Peizhi Zheng <pzheng16@asu.edu>
2020-07-25 18:57:45 -0700

    update

---  CheckIfItIsAStraightLine.py
+++  CheckIfItIsAStraightLine.py
@@ -13,25 +13,24 @@
 
 
 #0 division
-class Solution:
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        if len(coordinates) == 2:
-            return True
-        if coordinates[1][0] == coordinates[0][0]:
-            for i in range(2, len(coordinates)):
-                if coordinates[i][0] != coordinates[0][0]:
-                    return False
-        else:
-            a = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] -
-                                                           coordinates[0][0])
-            b = coordinates[0][1] - coordinates[0][0] * a
-            for i in range(2, len(coordinates)):
-                if coordinates[i][0] * a + b != coordinates[i][1]:
-                    return False
-        return True
-
-
 def checkStraightLine(coordinates):
+    if len(coordinates) == 2:
+        return True
+    if coordinates[1][0] == coordinates[0][0]:
+        for i in range(2, len(coordinates)):
+            if coordinates[i][0] != coordinates[0][0]:
+                return False
+    else:
+        a = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] -
+                                                        coordinates[0][0])
+        b = coordinates[0][1] - coordinates[0][0] * a
+        for i in range(2, len(coordinates)):
+            if coordinates[i][0] * a + b != coordinates[i][1]:
+                return False
+    return True
+
+
+def checkStraightLine1(coordinates):
     if len(coordinates) == 2:
         return True
     
