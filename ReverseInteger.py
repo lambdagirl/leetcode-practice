05606c819f6c689b723f95ed1411c8358e968b0d
Peizhi Zheng <pzheng16@asu.edu>
2020-07-27 06:47:39 -0700

    2 pointers

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -19,13 +19,12 @@ overflows => 0
 1.split str to list=>  reverse list (2 pointers)=> join
 2.num//10...100...1000 => 
 '''
-
-# def reverse(num):
+# #O(N)
+# def reverse3(num):
 #     ans = 0
 #     for i in range(1,len(str(num))+1):
 #         ans += num % (pow(10,i))
 #         num = num // (pow(10, i))
-#         print(num)
 #     return ans
 
 
@@ -36,6 +35,7 @@ def reverse(num):
     if num < 0:
         l = 1
     r = len(arr)-1
+
     while l < r:
         if l >= r:
             break
@@ -43,7 +43,10 @@ def reverse(num):
         l += 1
         r -= 1
     res = int(''.join(arr))
-    return 0 if res <= -pow(2, 31) or res > pow(2, 31)-1
+
+    if res <= -pow(2, 31) or (res > pow(2, 31)):
+        return 0
+
     return res
 
 
@@ -58,15 +61,15 @@ def reverse2(num):
     res = 0
 
     if num < 0:
-        sign = 0
+        sign = -1
         num = -num 
 
-    while True:
+    while num:
         res = res * 10 + num%10
         num = num // 10
-    return 0 if res > pow(2, 31) else res * symbol
-
+    return 0 if res > pow(2, 31) else res * sign
 
 print(reverse2(123))
 print(reverse2(-123))
 print(reverse2(10200))
+
