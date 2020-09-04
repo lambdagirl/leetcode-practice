1c60ddc4bdbe192fc56a7fe0a8c59ed90f1f8ed0
Peizhi Zheng <pzheng16@asu.edu>
2020-09-03 22:51:09 -0700

    matrix traverse

---
+++ spiral_matrix.py
@@ -0,0 +1,28 @@
+def spiralOrder(matrix):
+    if not matrix:
+        return []
+    R,C = len(matrix), len(matrix[0])
+    seen = [[False] * C for _ in matrix]
+    ans = []
+    dr = [0,1,0,-1]
+    dc = [1,0,-1,0]
+    r = c = di = 0
+
+    for _ in range( R * C):
+        ans.append(matrix[r][c])
+        seen[r][c] = True
+        cr, cc = r + dr[di], c + dc[di]
+
+        if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
+            r,c = cr, cc 
+        else:
+            di = (di+1)%4 
+            r, c = r + dr[di], c+ dc[di]
+    return ans
+
+matrix = [
+    [1, 2, 10],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(spiralOrder(matrix))
